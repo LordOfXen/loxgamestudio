cfd1fd5834461f5ad5d99ca9c434fc5e36b6c22b
LordOf Xen <support@mail.lgamestudio.com>
2019-08-08 14:08:52 -0700

    Delete servers.js

--- bots/advertisingbot/panel_animation/servers.js
+++
@@ -1,6 +0,0 @@
-var discordServers = [];
-
-discordServers.push({ name: 'Notarged 2', image: 'notarged2.png', verified: true, hiddenbackground: true });
-discordServers.push({ name: 'Lox Test Server', image: 'discord.png', verified: false, hiddenbackground: true });
-discordServers.push({ name: 'Lox Game Studios', image: 'lgs.png', verified: true, hiddenbackground: true });
-discordServers.push({ name: 'Advertising Bot Testings', image: 'guild.png', verified: false, hiddenbackground: true });
