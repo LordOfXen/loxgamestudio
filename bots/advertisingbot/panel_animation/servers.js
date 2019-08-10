2eb6255a3b1d6c14e926f4d618a122afbf486f6f
LordOf Xen <support@mail.lgamestudio.com>
2019-08-09 17:44:11 -0700

    Update servers.js

--- bots/advertisingbot/panel_animation/servers.js
+++ bots/advertisingbot/panel_animation/servers.js
@@ -1,8 +1,8 @@
 var discordServers = [];
 
 discordServers.push({ name: 'Notarged 2', image: 'notarged2.png', verified: true, hiddenbackground: true });
+discordServers.push({ name: 'DarkenedSouls', image: 'servers/509297478869319683.png', verified: true, hiddenbackground: true });
 discordServers.push({ name: "Coco's Roleplay Server", image: 'servers/417225375349997578.png', verified: false, hiddenbackground: true });
 discordServers.push({ name: 'Lox Game Studios', image: 'lgs.png', verified: true, hiddenbackground: true });
-discordServers.push({ name: 'DarkenedSouls', image: 'servers/509297478869319683.png', verified: false, hiddenbackground: true });
 discordServers.push({ name: 'worlork', image: 'servers/185874277537546242.png', verified: false, hiddenbackground: true });
 discordServers.push({ name: 'Leo Corporation', image: 'servers/607640410638123064.jpg', verified: false, hiddenbackground: true });
